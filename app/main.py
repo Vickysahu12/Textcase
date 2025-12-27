35eefc523494465cbfb71c8f9a9f1bc412b0c608
Vickysahu12 <bikisahu161@gmail.com>
2025-12-27 20:38:20 +0530

    made a route

--- app/main.py
+++ app/main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def main():
     return {"data":"creating a new project named Textcasepro"}
+
+@app.get("/vicky")
+async def me():
+    return{"data":"Hey guys I am vicky and I am making an app that can help to the CAT ASPIRANT FOR CRACKING THE CAT"}
