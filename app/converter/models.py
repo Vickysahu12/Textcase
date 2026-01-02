69e01e236b23f730d98f3b10eff78f738fc5bf64
Vickysahu12 <bikisahu161@gmail.com>
2026-01-02 16:54:07 +0530

    create a class UserCredits

--- app/converter/models.py
+++ app/converter/models.py
@@ -1,2 +1,11 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, Integer, ForeignKey, DateTime
+from datetime import datetime, timezone
+from app.db.config import Base
+
+class UserCredits(Base):
+    __tablename__ = "user_credits"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"), unique=True)
+    credits: Mapped[int] = mapped_column(Integer, default=10)
--- app/main.py
+++ app/main.py
@@ -18,13 +18,3 @@ async def main():
         "me":"Hey Guys I am vickyyy",
         "dream":"I want to be an entreprenuer and crack the cat 2026"
     }
-
-async def me():
-     return{
-          "vicky":"Hyy guys I am vicky you are seeing the richest person of odisha"
-     }
-
-async def vicky():
-     return{
-          "vicky":"Hyy guys I am vicky you are seeing the richest person of odisha"
-     }
